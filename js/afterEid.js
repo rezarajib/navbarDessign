d9a0e177e1b6dc6f2548fc0d454c2a2af3513e00
Rajib Reza <rajibreza64@gmail.com>
2025-04-04 00:49:48 +0600

    posted-date

--- js/afterEid.js
+++ js/afterEid.js
@@ -22,10 +22,15 @@ const displayVideo = (video) => {
         const card = document.createElement('div');
         card.classList = 'card bg-base-100 w-96 shadow-sm';
         card.innerHTML = `
+        <figure class = "h-[200] relative">
         <img
       src="${elementId.thumbnail}"
       class = "w-full h-full object-cover"
       alt="Shoes" />
+      <span class="absolute bg-gray-800 text-white rounded p-2 right-2 bottom-2">
+    ${elementId.others.posted_date}
+</span>
+
   </figure>
   <div class="flex items-center gap-10 p-3">
     <!-- Profile Image -->
@@ -35,10 +40,13 @@ const displayVideo = (video) => {
     <div>
         <h2 class="font-bold text-lg">${elementId.title}</h2>
 
-        <div class="flex items-center gap-1 text-gray-500 text-sm">
+        <div class="flex items-center gap-4 text-gray-500 text-sm">
             <p>${elementId.authors[0].profile_name}</p>
+            ${elementId.authors[0].verified === true
+                ? `<img src='https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png'/>` 
+                : ""}
             <!-- Verified Badge -->
-            <img class="w-4 h-4" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" alt="Verified">
+         
         </div>
 
         <p class="text-gray-400 text-sm">${elementId.others.views} views</p>
